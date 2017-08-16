5fca8996398ab26df9361d51c6391cd42199d605
craneplus <31060325+craneplus@users.noreply.github.com>
2017-08-16 14:12:08 +0530

    Create 2017_08_12_112808_create_counter_table.php

---
+++ src/migrations/2017_08_12_112808_create_counter_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCounterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counter', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('class_name');
+            $table->integer('object_id');
+            $table->integer('view_counter');
+            $table->integer('like_counter');
+      
+            $table->index('class_name');
+            $table->index('object_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('counter');
+    }
+}
