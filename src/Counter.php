6e0917706c2ee1aa564223ece06791b792c144b7
craneplus <31060325+craneplus@users.noreply.github.com>
2017-08-16 12:59:01 +0530

    Update Counter.php

--- src/Counter.php
+++ src/Counter.php
@@ -1,1 +1,14 @@
 
+<?php
+
+namespace craneplus\view-counter;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Counter extends Model
+{
+  protected $fillable = [
+  	'class_name',
+  	'object_id'
+  ];
+}
